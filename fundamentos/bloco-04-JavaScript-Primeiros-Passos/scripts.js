e68b8776c0663666b6afba470334c277c9c10aff
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2021-11-27 01:55:32 -0300

    let and const

--- fundamentos/bloco-04-JavaScript-Primeiros-Passos/scripts.js
+++ fundamentos/bloco-04-JavaScript-Primeiros-Passos/scripts.js
@@ -1,3 +1,6 @@
-let age = 20;
-
+const age = 20; //uma constante não pode ter seu valor mudado
+age = 50;
+let fullName = "Alex"; //camellCase
+fullName = "Horman";
 console.log(age);
+console.log(fullName);
