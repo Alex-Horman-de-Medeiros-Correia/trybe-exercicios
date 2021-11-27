8bf457fe2ca6741a4543ae5d763455a98863575b
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2021-11-27 02:03:36 -0300

    let and const 2

--- fundamentos/bloco-04-JavaScript-Primeiros-Passos/scripts.js
+++ fundamentos/bloco-04-JavaScript-Primeiros-Passos/scripts.js
@@ -1,6 +1,9 @@
-const age = 20; //uma constante não pode ter seu valor mudado
-age = 50;
-let fullName = "Alex"; //camellCase
-fullName = "Horman";
-console.log(age);
-console.log(fullName);
+const myName = "Alex";
+const birthCity = "João Pessoa";
+birthCity = "Itabaiana"; //recebemos uma mensagem de erro pq uma constante permanece fiel à sua atribuição inicial.
+let birthYear = 1990;
+birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
