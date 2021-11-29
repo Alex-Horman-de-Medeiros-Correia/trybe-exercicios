b4d17cb38e9b01cc5513b130aaa941b0d28b6987
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2021-11-29 02:02:40 -0300

    if and else

---
+++ fundamentos/bloco-04-introdução-a-javascript-e-logica-da-programação/dia-01-JavaScript-Primeiros-Passos/scripts.js
@@ -0,0 +1,12 @@
+
+let trybe = 14;
+
+if (trybe >= 14 && trybe < 14.40) {
+    console.log("esquenta");
+}else if (trybe >= 16.20 && trybe < 18) {
+    console.log("aula ao vivo");
+}else if(trybe >= 19.40 && trybe < 20) {
+    console.log("fechamento");
+}else{
+    console.log("momento assincrono");
+}
