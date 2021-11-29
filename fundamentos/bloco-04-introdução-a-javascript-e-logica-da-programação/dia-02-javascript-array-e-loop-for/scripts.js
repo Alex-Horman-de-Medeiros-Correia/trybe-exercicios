af2ca6c62c890509acd77c612e105572ec4624e2
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2021-11-29 18:35:30 -0300

    propriedades do array

---
+++ fundamentos/bloco-04-introdução-a-javascript-e-logica-da-programação/dia-02-javascript-array-e-loop-for/scripts.js
@@ -0,0 +1,30 @@
+
+let atividadesDoDia = ['comer', 'correr', 'tomar banho', 'estudar'];
+
+//let primeiraAtividade = atividadesDoDia[0];
+    //console.log(primeiraAtividade);
+
+atividadesDoDia.push ('dormir enfim');
+
+atividadesDoDia.unshift ('lavar rosto');
+
+atividadesDoDia.pop();
+
+atividadesDoDia.shift();
+
+let indexAtividades = atividadesDoDia.indexOf('estudar');
+    console.log(indexAtividades);
+
+    //console.log(atividadesDoDia);
+
+
+//for (let index = 0; index < atividadesDoDia.length; index += 1) {
+    //console.log(atividadesDoDia[index]);
+//}
+
+
+
+
+
+
+
