d8c66ec3d06021aea7c0e5e3feb66be4cad89f8f
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2021-11-30 15:28:33 -0300

    verficando o numero maior

--- fundamentos/bloco-04-introdução-a-javascript-e-logica-da-programação/dia-02-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introdução-a-javascript-e-logica-da-programação/dia-02-javascript-array-e-loop-for/scripts.js
@@ -1,10 +1,15 @@
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let nomesSeparados of names) {
-    console.log(nomesSeparados);
+let numeroMaisAlto = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] > numeroMaisAlto) {
+        numeroMaisAlto = numbers[index];
+    }
+    
 }
 
-
+console.log(numeroMaisAlto);
 
 
 
