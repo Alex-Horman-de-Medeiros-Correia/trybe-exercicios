4e21eea35e07bb0746c51d6568d65abb1b92867f
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2021-11-30 00:02:15 -0300

    um pouco de array e for

--- fundamentos/bloco-04-introdução-a-javascript-e-logica-da-programação/dia-02-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introdução-a-javascript-e-logica-da-programação/dia-02-javascript-array-e-loop-for/scripts.js
@@ -1,10 +1,9 @@
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let convidados = ['marla', 'hilda', 'claudio', 'goku', 'batman'];
 
-menu.push('Sobre nós');
-
-menu.unshift('Curiosidades');
-
-console.log(menu);
+for (var indice = 0; indice < convidados.length; indice += 1) {
+    var mensagem = 'boas vindas, ' + convidados[indice] + ', e boas festas!'; //aqui concatenamos strings com a variavel seguida do indice correspondente
+    console.log(mensagem);
+}
 
 
 
