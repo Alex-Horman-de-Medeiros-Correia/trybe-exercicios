2b34ab8de92819751ff701f36aba8f6884a7748a
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2021-12-01 02:04:49 -0300

    logica de programaçao

--- fundamentos/bloco-04-introdução-a-javascript-e-logica-da-programação/dia-03-logica-de-programação-e-algoritmos/script.js
+++ fundamentos/bloco-04-introdução-a-javascript-e-logica-da-programação/dia-03-logica-de-programação-e-algoritmos/script.js
@@ -1,14 +1,11 @@
-//exercise.js
-let fruits = [3, 4, 10, 1, 12];
+let n = 4;
+let simbolo = '*';
+let colocarLinha = '';
 
-let resultado = 0;
-
-for (let index = 0; index < fruits.length; index += 1) {
-    resultado += fruits[index];
+for (let index = 0; index < n; index += 1) {
+    colocarLinha = colocarLinha + simbolo;
 }
 
-if (resultado > 15) {
-    console.log(resultado);
-}else{
-    console.log("valor menor que 16")
+for (let index = 0; index < n; index += 1) {
+    console.log(colocarLinha);
 }
