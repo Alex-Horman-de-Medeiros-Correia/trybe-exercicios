c851e2e4caf6fea5c1fd02c2e9280196fe5ac6ba
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2021-11-30 22:19:49 -0300

    baby steps

---
+++ fundamentos/bloco-04-introdução-a-javascript-e-logica-da-programação/dia-03-logica-de-programação-e-algoritmos/script.js
@@ -0,0 +1,14 @@
+let fruits = [3, 4, 10, 1, 12];
+let resultado = 0;
+for (let index = 0; index < fruits.length; index +=1) {
+    resultado += fruits[index];
+    
+}
+
+//console.log(resultado);
+
+if (resultado > 15){
+    console.log(resultado);
+}else{
+    console.log('valor menor que 16')
+}
