0a7f7c121ac77e34491b8ad2b0b75de06657e4b2
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2021-12-03 16:16:52 -0300

    aprendendo getElementById

---
+++ fundamentos/bloco-05-DOM-e-seletores/script.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById("page-title").innerText = "Pulp Fiction";
+
+document.getElementById("second-paragraph").innerText = "O melhor filme do Tarantino, folks!!!";
+
+document.getElementById("subtitle").innerText = "Tempo de Violência... Como eram ruins os subtítulos dos anos 90";
