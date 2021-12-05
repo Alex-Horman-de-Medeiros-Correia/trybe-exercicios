e46a967f622d7a849d0a6f3a72e32e428c1cabd8
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2021-12-05 02:29:22 -0300

    exercicio

---
+++ fundamentos/bloco-05-DOM-e-seletores/script2.js
@@ -0,0 +1,35 @@
+document.getElementById("container").style.backgroundColor = 'grey';
+
+document.querySelector("#header-container h1").style
+
+
+
+document.querySelector("#header-container").style.border = '5px solid black';
+
+document.querySelector("#header-container").style.background = 'green';
+
+document.querySelector('.emergency-tasks h3').style.background = 'green';
+
+
+/* document.querySelector('.emergency-tasks h3').style.padding = '40px 0'; */
+
+/* document.querySelector('.emergency-tasks').firstElementChild.style.background = 'red'; */
+
+document.querySelector('.emergency-tasks div').style.background = 'red';
+
+document.querySelectorAll('.emergency-tasks div')[1].style.background = 'pink';
+
+document.querySelectorAll('.emergency-tasks h3')[1].style.background = 'green';
+
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.background = 'green';
+
+document.querySelectorAll(".no-emergency-tasks div")[0].style.background = 'yellow';
+
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.background = 'green';
+
+document.querySelectorAll(".no-emergency-tasks div")[1].style.background = 'blue';
+
+document.getElementById("footer-container").style.backgroundColor = 'black';
+
+
+
