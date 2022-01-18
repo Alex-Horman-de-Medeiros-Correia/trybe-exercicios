e38190018f929b9c6e6755688657ba5eddd5bbb9
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2022-01-17 22:02:13 -0300

    sommationOf passou com todos os testes

--- fundamentos/bloco-07-javascriptES6-e-teste-unitarios/dia-03-primeiros-passos-em-jest/loginValidation.js
+++ fundamentos/bloco-07-javascriptES6-e-teste-unitarios/dia-03-primeiros-passos-em-jest/loginValidation.js
@@ -1,4 +1,4 @@
-const numeros = (array) => {
+/* const numeros = (array) => {
   for (let index = 0; index < array.length; index += 1){
     if(typeof array[index] !== 'number'){
       return false;
@@ -7,5 +7,29 @@ const numeros = (array) => {
   return true;
 };
 
-module.exports = numeros;
+module.exports = numeros; */
+
+
+/* const removeBebida = (array, remover) => {
+  array.splice(array.indexOf(`${remover}`), 1);
+  return array;
+};
+
+module.exports = removeBebida; */
+
+//testes unitários - códigos que testam uma unidade de código. um trecho de código. Uma função.
+
+/* const sum = (para1, para2) => para1 + para2;
+
+module.exports = sum; */
+
+const sommationOf = (number) => {
+  let soma = 0;
+  for (let index = 1; index <= number; index += 1){
+    soma += index;
+  }
+  return soma;
+}
+
+module.exports = sommationOf;
 
--- fundamentos/bloco-07-javascriptES6-e-teste-unitarios/dia-03-primeiros-passos-em-jest/loginValidation.test.js
+++ fundamentos/bloco-07-javascriptES6-e-teste-unitarios/dia-03-primeiros-passos-em-jest/loginValidation.test.js
@@ -1,4 +1,4 @@
-const numeros = require('./loginValidation');
+/* const numeros = require('./loginValidation');
 
 //describe corresponde a um bloco onde diversos testes podem ser agrupados, organizando assim diferentes grupos caso necessário.
 
@@ -22,4 +22,54 @@ describe('requisito 3', () => {
   test(`a função recebe [' '] e retorna false`, () => {
     expect(false).toEqual(numeros([' ']));
   });
+}); */
+
+/* const removeBebida = require('./loginValidation');
+
+describe('remove sabor de bebida', () => {
+  test ('remover agua', () => {
+    const bebidas = ['suco', 'agua', 'refri', 'vinho', 'ksuqui', 'groselha', 'garapa'];
+    expect(['suco', 'refri', 'vinho', 'ksuqui', 'groselha', 'garapa']).toEqual(removeBebida(bebidas, 'agua'));
+  });
+
+  test('remover garapa', () => {
+    const bebidas = ['suco', 'agua', 'refri', 'vinho', 'ksuqui', 'groselha', 'garapa'];
+    expect(['suco', 'agua', 'refri', 'vinho', 'ksuqui', 'groselha']).toEqual(removeBebida(bebidas, 'garapa'));
+  });
+
+  test('remover groselha caso exista', () => {
+    const bebidas = ['suco', 'agua', 'refri', 'vinho', 'ksuqui', 'groselha', 'garapa'];
+    expect(removeBebida(bebidas, 'groselha')).not.toContain('groselha');
+  });
+}); */
+
+
+/* const sum = require('./loginValidation');
+const soma = require('./loginValidation');
+
+test ('adiciona 1 e 2 e retorna 3', () => {
+  expect(soma(1,2)).toBe(3);
+});
+
+test('adciona -33 e 23 e retorna -10', () => {
+  expect(soma(-33, 23)).toBe(-10);
+}); */
+
+
+const sommationOf = require('./loginValidation');
+
+test ('testar se a função sommationOf existe', () => {
+  expect(typeof sommationOf).toBe('function');
+});
+
+test ('espera que a somatoria de 1 seja 1', () => {
+  expect(sommationOf(1)).toBe(1);
+});
+
+test ('espera que a somatoria de 3 seja 6', () => {
+  expect(sommationOf(3)).toBe(6);
+});
+
+test ('espera que a somatoria de 5 seja 15', () => {
+  expect(sommationOf(5)).toBe(15);
 });
