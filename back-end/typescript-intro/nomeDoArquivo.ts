62fb9b23e1b2da7ec760695b28524a15864febe4
Alex-Horman-de-Medeiros-Correia <alexhorman1990@gmail.com>
2022-10-07 21:44:35 -0300

    conceitos básicos de tipos

---
+++ back-end/typescript-intro/nomeDoArquivo.ts
@@ -0,0 +1,17 @@
+let x:number = 2;
+
+let y: number = 3;
+
+console.log(x + y);
+
+let yes: boolean = true;
+
+let no: boolean = false;
+
+function sayHello(): void {
+    console.log('hello world');
+    
+}
+let s: string = 'alex';
+
+let one = 1; //inferencia de tipo
