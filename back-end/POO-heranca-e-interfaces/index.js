db3d72728bbf69cfbabf1e7271ccaf4d4a441411
Alex-Horman-de-Medeiros-Correia <alexhorman1990@gmail.com>
2022-10-23 19:21:09 -0300

    estudando composição

--- back-end/POO-heranca-e-interfaces/index.js
+++ back-end/POO-heranca-e-interfaces/index.js
@@ -1,11 +1,26 @@
 "use strict";
-class myClass {
-    constructor(myNumber) {
-        this.myNumber = myNumber;
-    }
-    myFunc(myParam) {
-        return `${myParam + this.myNumber} é o resultado!`;
+class ConsoleLogger {
+    log(param) {
+        console.log(param);
     }
 }
-const num = new myClass(32);
-console.log(num.myFunc(30));
+class ConsoleLogger2 {
+    log(param) {
+        console.log(`logger 2: ${param}`);
+    }
+}
+class ExampleDatabase {
+    constructor(Logger) {
+        this.Logger = Logger;
+        this.Logger = new ConsoleLogger();
+    }
+    save(key, value) {
+        this.Logger.log(`salvando o valor ${value} passado para a chave ${key}`);
+    }
+}
+const log1 = new ConsoleLogger();
+const log2 = new ConsoleLogger2();
+const data1 = new ExampleDatabase(log1);
+const data2 = new ExampleDatabase(log2);
+data1.save('key 1', 'anonimus');
+data2.save('key 2', 'tenia');
--- back-end/POO-heranca-e-interfaces/index.ts
+++ back-end/POO-heranca-e-interfaces/index.ts
@@ -1,21 +1,49 @@
-interface MyInterface {
-    myNumber: number,
-    myFunc(myParam: number): string,
+interface Logger {
+    log(param: string): void,
 }
 
-class myClass implements MyInterface {
-    constructor(public myNumber: number) {}
+class ConsoleLogger implements Logger {
 
-    myFunc(myParam: number): string {
-        return `${ myParam + this.myNumber } é o resultado!`
+    log(param: string) {
+        console.log(param);
+    }
+
+}
+
+class ConsoleLogger2 implements Logger {
+
+    log(param: string) {
+        console.log(`logger 2: ${ param }`);
+    }
+
+}
+
+interface Database {
+    Logger: Logger,
+    save(key: string, value: string): void,
+}
+
+class ExampleDatabase implements Database {
+    constructor(public Logger: Logger) { this.Logger = new ConsoleLogger() }
+
+    public save(key: string, value: string) {
+        this.Logger.log(`salvando o valor ${ value } passado para a chave ${ key }`);
     }
 }
 
+const log1 = new ConsoleLogger();
+const log2 = new ConsoleLogger2();
+
+const data1 = new ExampleDatabase(log1);
+const data2 = new ExampleDatabase(log2);
 
 
-const num = new myClass(32);
+data1.save('key 1', 'anonimus');
+data2.save('key 2', 'tenia');
 
-console.log(num.myFunc(30));
+
+
+
 
 
 
