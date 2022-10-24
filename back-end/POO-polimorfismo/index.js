9d74d7a8c566deab2bdf2558bb2f657ccb9dbe73
Alex-Horman-de-Medeiros-Correia <alexhorman1990@gmail.com>
2022-10-24 00:36:49 -0300

    conceitos iniciais de polimorfismo

---
+++ back-end/POO-polimorfismo/index.js
@@ -0,0 +1,35 @@
+"use strict";
+class Animal {
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    ;
+    mover() {
+        console.log(`o ${this.nome} está se movendo`);
+    }
+    age() {
+        console.log(`o ${this.nome} possui ${this.idade} anos de idade`);
+    }
+}
+class felino extends Animal {
+    mover() {
+        super.age();
+        console.log(`o ${this.nome} está ronronando`);
+    }
+}
+class inseto extends Animal {
+    mover() {
+        super.mover(); // aqui o método mover se mantém intacto conforme definido na superclasse
+        console.log(`a ${this.nome} está zumbindo`);
+    }
+}
+const an = new Animal('humano', 100);
+const fe = new felino('gato', 10);
+const ins = new inseto('abelha', 1);
+const myFunc = (animal) => {
+    return animal.mover();
+};
+myFunc(an);
+myFunc(fe);
+myFunc(ins);
