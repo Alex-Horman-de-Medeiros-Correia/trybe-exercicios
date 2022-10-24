9d1c119c51fc39012e6c9de9767b08400576781a
Alex-Horman-de-Medeiros-Correia <alexhorman1990@gmail.com>
2022-10-24 20:24:16 -0300

    implementando interfaces e generics segundo o polimorfismo

--- back-end/POO-polimorfismo/index.js
+++ back-end/POO-polimorfismo/index.js
@@ -1,37 +1,63 @@
 "use strict";
-class Character {
-}
-class MeleeCharacter extends Character {
-    constructor(nome, special) {
-        super();
-        this.nome = nome;
-        this.special = special;
+class PessoaFisica {
+    constructor(name, cpf) {
+        this._id = PessoaFisica.newId();
+        this._name = name;
+        this._cpf = cpf;
     }
-    talk() {
-        console.log(`o personagem ${this.nome} está falando`);
+    static newId() {
+        return this.lastId++;
     }
-    specialMove() {
-        console.log(`poder especial de curto alcance: ${this.special}`);
-    }
-}
-class LongRangeCharacter extends Character {
-    constructor(nome, special) {
-        super();
-        this.nome = nome;
-        this.special = special;
-    }
-    talk() {
-        console.log(`o personagem ${this.nome} está falando`);
-    }
-    specialMove() {
-        console.log(`poder especial de longo alcance: ${this.special}`);
+    get id() { return this._id; }
+    ;
+    get name() { return this._name; }
+    ;
+    get cpf() { return this._cpf; }
+    ;
+    showIdentification() {
+        console.log(this.id, this._cpf);
     }
 }
-const myFunc = (character) => {
-    character.talk();
-    character.specialMove();
+PessoaFisica.lastId = 0;
+class PessoaJur {
+    constructor(name, cnpj) {
+        this._id = PessoaJur.newId();
+        this._name = name;
+        this._cnpj = cnpj;
+    }
+    static newId() {
+        return this.lastId++;
+    }
+    get id() { return this._id; }
+    ;
+    get name() { return this._name; }
+    ;
+    get cnpj() { return this._cnpj; }
+    ;
+    showIdentification() {
+        console.log(this.id, this._cnpj);
+    }
+}
+PessoaJur.lastId = 0;
+class Contrato {
+    constructor(corretor) {
+        this.corretor = corretor;
+    }
+    static get number() { return this._number; }
+}
+Contrato._number = 0;
+const pf1 = new PessoaFisica('alex', '6666666666');
+const pf2 = new PessoaFisica('andre', '77777777777');
+const pj1 = new PessoaJur('aledrethur', '9999999999');
+const pj2 = new PessoaJur('ltda barra', '55555555555');
+const myfunc = (identification) => {
+    identification.showIdentification();
 };
-const curto = new MeleeCharacter('Sagat', 'laminas');
-const longo = new LongRangeCharacter('Ryu', 'haduken');
-myFunc(curto);
-myFunc(longo);
+myfunc(pf1);
+myfunc(pf2);
+myfunc(pj1);
+myfunc(pj2);
+const c1 = new Contrato(pf1);
+console.log(`${c1.corretor.name} possui contrato ativo, de cpf ${c1.corretor.cpf}`);
+const c2 = new Contrato(pj1);
+console.log(`${c2.corretor.name} possui contrato ativo, de cpf ${c2.corretor.cnpj}`);
--- back-end/POO-polimorfismo/index.ts
+++ back-end/POO-polimorfismo/index.ts
@@ -1,43 +1,84 @@
-abstract class Character {
-    abstract talk(): void;
-    abstract specialMove(): void;
+interface Pessoa {
+    id: number,
+    name: string,
+    showIdentification(): void,
 }
 
-class MeleeCharacter extends Character {
-    constructor(public nome: string, private special: string) {
-        super();
+class PessoaFisica implements Pessoa {
+    private static lastId = 0;
+    private _name;
+    private _id;
+    private _cpf;
+
+    constructor(name: string, cpf: string) {
+        this._id = PessoaFisica.newId();
+        this._name = name;
+        this._cpf = cpf;
     }
 
-    talk(): void {
-        console.log(`o personagem ${ this.nome } está falando`);
+    private static newId() {
+        return this.lastId++;
     }
 
-    specialMove(): void {
-        console.log(`poder especial de curto alcance: ${ this.special }`);
-    }
-}
+    get id() { return this._id };
+    get name() { return this._name };
+    get cpf() { return this._cpf };
 
-class LongRangeCharacter extends Character {
-    constructor(public nome: string, private special: string) {
-        super();
-    }
-
-    talk(): void {
-        console.log(`o personagem ${ this.nome } está falando`);
-    }
-
-    specialMove(): void {
-        console.log(`poder especial de longo alcance: ${ this.special }`);
+    showIdentification(): void {
+        console.log(this.id, this._cpf);
     }
 }
 
-const myFunc = (character: Character) => {
-    character.talk();
-    character.specialMove();
+class PessoaJur implements Pessoa {
+    private static lastId = 0;
+    private _name;
+    private _id;
+    private _cnpj;
+
+    constructor(name: string, cnpj: string) {
+        this._id = PessoaJur.newId();
+        this._name = name;
+        this._cnpj = cnpj;
+    }
+
+    private static newId() {
+        return this.lastId++;
+    }
+
+    get id() { return this._id };
+    get name() { return this._name };
+    get cnpj() { return this._cnpj };
+
+    showIdentification(): void {
+        console.log(this.id, this._cnpj);
+    }
 }
 
-const curto = new MeleeCharacter('Sagat', 'laminas');
-const longo = new LongRangeCharacter('Ryu', 'haduken');
+class Contrato<T> {
+    static _number = 0;
 
-myFunc(curto);
-myFunc(longo);
+    constructor(public corretor: T) {}
+
+    static get number() { return this._number }
+}
+
+
+const pf1 = new PessoaFisica('alex', '6666666666');
+const pf2 = new PessoaFisica('andre', '77777777777');
+const pj1 = new PessoaJur('aledrethur', '9999999999');
+const pj2 = new PessoaJur('ltda barra', '55555555555');
+
+const myfunc = (identification: Pessoa) => {
+    identification.showIdentification();
+}
+
+myfunc(pf1);
+myfunc(pf2);
+myfunc(pj1);
+myfunc(pj2);
+
+const c1 = new Contrato(pf1);
+console.log(`${ c1.corretor.name } possui contrato ativo, de cpf ${ c1.corretor.cpf }`);
+
+const c2: Contrato<PessoaJur> = new Contrato(pj1);
+console.log(`${ c2.corretor.name } possui contrato ativo, de cnpj ${ c2.corretor.cnpj }`);
