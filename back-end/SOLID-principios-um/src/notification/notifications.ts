6d8523882c59831cc78178926d71e356c1ccafed
Alex-Horman-de-Medeiros-Correia <alexhorman1990@gmail.com>
2022-10-29 22:03:11 -0300

    SOLID - PRINCIPIO ABERTO/FECHADO

--- back-end/SOLID-principios-um/src/notification/notifications.ts
+++ back-end/SOLID-principios-um/src/notification/notifications.ts
@@ -1,3 +1,34 @@
-export default function progressNotification(message: string): void {
-    console.log(message);
-  }
+type notification = { type: string, send: (message: string) => void };
+
+const consoleNotification: notification = {
+  type: 'Console',
+  send: (message) => console.log(`Console: ${message}`),
+};
+
+const emailNotification: notification = {
+  type: 'Email',
+  send: (message) => console.log(`Email: ${message}`),
+};
+
+const phoneNotification: notification = {
+  type: 'Phone',
+  send: (message) => console.log(`Phone: ${message}`),
+};
+
+const notiTogether: notification[] = [
+  consoleNotification,
+  emailNotification,
+  phoneNotification,
+];
+
+export default function progressNotification(
+  message: string,
+  typeZero = 'Console',
+  notificationArray: notification[] = notiTogether,
+): void {
+  notificationArray.forEach((notificaion) => {
+    if (notificaion.type === typeZero) {
+      notificaion.send(message);
+    }
+  });
+}
